eb965617687e1899a7614cf2dadb1b0ea0c83b67
RIATtm <riatltd@gmail.com>
2013-08-19 13:47:09 +0530

    "Class Added"

---
+++ VIVA/src/model/testing.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Win -8
+ */
+public class testing {
+    public void check(){
+        String s="u r best mchooo!!!";
+        System.out.println(s);
+    }
+}
