dc9ebd256ebbbc00c79128a1d348f53dd4654eb9
RIAT <riatltd@gmail.com>
2013-08-19 13:42:47 +0530

    "testing"

--- VIVA/src/model/Alignment.java
+++ VIVA/src/model/Alignment.java
@@ -16,5 +16,6 @@ public class Alignment {
         l[1].setBounds(80, 170, 727, 330);
         l[2].setBounds(450, 110, 727, 440);
         l[3].setBounds(560, 170, 727, 330);
+        System.out.println("Devon, hw r you????");
     }
 }
